ed9374e9a4fe786355eb74fbf2f16597895e8c44
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-13 15:38:33 +0100

    refactored navbar and booklist to use ncontext consumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,22 +2,28 @@ import React, { Component } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 class BookList extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLight, light, dark } = this.context;
-    console.log(this.context);
-    const theme = isLight ? light : dark;
     return (
-      <div
-        className="book-list"
-        style={{ color: theme.font, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The way of kings</li>
-          <li style={{ background: theme.ui }}>The name of the Wind </li>
-          <li style={{ background: theme.ui }}>The final Empire</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLight, light, dark } = context;
+
+          const theme = isLight ? light : dark;
+
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.font, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>The way of kings</li>
+                <li style={{ background: theme.ui }}>The name of the Wind </li>
+                <li style={{ background: theme.ui }}>The final Empire</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,25 @@ import React, { Component } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLight, light, dark } = this.context;
-    console.log(this.context);
-    const theme = isLight ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.font }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>CONTACT</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLight, light, dark } = context;
+
+          const theme = isLight ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.font }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
