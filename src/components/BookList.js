ebff44211e5152e44139058727eede29e405f767
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-13 15:11:41 +0100

    styling and theming with context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLight, light, dark } = this.context;
+    console.log(this.context);
+    const theme = isLight ? light : dark;
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ color: theme.font, background: theme.bg }}
+      >
         <ul>
-          <li>The way of kings</li>
-          <li>The name of the Wind </li>
-          <li>The final Empire</li>
+          <li style={{ background: theme.ui }}>The way of kings</li>
+          <li style={{ background: theme.ui }}>The name of the Wind </li>
+          <li style={{ background: theme.ui }}>The final Empire</li>
         </ul>
       </div>
     );
