d066d4a9ba539767ef8d9db657731c1a32daee9e
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-13 16:56:24 +0100

    some progress with authentication toggling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,34 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import { AuthContext } from "../context/AuthContext";
 
 class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>
-        {context => {
-          const { isLightTheme, light, dark } = context;
-
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.font }}>
-              <h1>Context App</h1>
-              <ul>
-                <li>HOME</li>
-                <li>ABOUT</li>
-                <li>CONTACT</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {authContext => (
+          <ThemeContext.Consumer>
+            {themeContext => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.font }}>
+                  <h1>Context App</h1>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged in" : "Logged out"}
+                  </div>
+                  <ul>
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
