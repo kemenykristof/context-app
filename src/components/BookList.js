b97196851de6a0569db30fd473850306d43a16f7
kemenykristof <kemeny.kristof24@gmail.com>
2019-11-14 11:02:20 +0100

    booklist refactored to use hooks

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,31 +1,21 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-class BookList extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {context => {
-          const { isLightTheme, light, dark } = context;
-
-          const theme = isLightTheme ? light : dark;
-
-          return (
-            <div
-              className="book-list"
-              style={{ color: theme.font, background: theme.bg }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>The way of kings</li>
-                <li style={{ background: theme.ui }}>The name of the Wind </li>
-                <li style={{ background: theme.ui }}>The final Empire</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.font, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>The way of kings</li>
+        <li style={{ background: theme.ui }}>The name of the Wind </li>
+        <li style={{ background: theme.ui }}>The final Empire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
